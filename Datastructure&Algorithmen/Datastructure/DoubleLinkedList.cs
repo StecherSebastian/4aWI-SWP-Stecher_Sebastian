730a9db9519c8f8dc20a2a68e40fd9cd887b93db
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2025-10-11 10:09:39 +0200

    addedDoubleLinkeListAddFirstAndLast

---
+++ Datastructure&Algorithmen/Datastructure/DoubleLinkedList.cs
@@ -0,0 +1,46 @@
+﻿using Common;
+
+namespace Datastructure
+{
+    public class DoubleLinkedList<T>
+    {
+        private Node<T>? _Head;
+        private Node<T> _Last = null!;
+        public void AddFirst(T data)
+        {
+            Node<T> toAdd = new(data);
+            toAdd.Next = _Head;
+            if (_Head != null)
+                _Head.Previous = toAdd;
+            _Head = toAdd;
+            if (_Last == null)
+                _Last = _Head;
+        }
+        public void AddLast(T data)
+        {
+            Node<T> toAdd = new(data);
+            if (_Head == null)
+            {
+                _Head = toAdd;
+                _Last = _Head;
+            }
+            else
+            {
+                _Last.Next = toAdd;
+                toAdd.Previous = _Last;
+                _Last = toAdd;
+            }
+        }
+        public List<T> GetAllNodesData()
+        {
+            List<T> values = new();
+            Node<T>? current = _Head;
+            while (current != null)
+            {
+                values.Add(current.Data);
+                current = current.Next;
+            }
+            return values;
+        }
+    }
+}
---
+++ Datastructure&Algorithmen/DatastructureTests/DoubleLinkedListTests.cs
@@ -0,0 +1,63 @@
+﻿using Datastructure;
+
+namespace DatastructureTests
+{
+    public class DoubleLinkedListTests
+    {
+        private Person _Person1;
+        private Person _Person2;
+        private Person _Person3;
+        private Person? _PersonNull;
+        [SetUp]
+        public void Setup()
+        {
+            _Person1 = new("Sebastian");
+            _Person2 = new("Lukas");
+            _Person3 = new("Stephan");
+            _PersonNull = null;
+        }
+        [Test]
+        public void AddFirst_AddMultipleObjects_ObjectsInCorrectOrder()
+        {
+            DoubleLinkedList<Person> linkedList = new();
+            linkedList.AddFirst(_Person1);
+            linkedList.AddFirst(_Person2);
+            linkedList.AddFirst(_Person3);
+            Assert.That(linkedList.GetAllNodesData(), Is.EqualTo(new List<Person> { _Person3, _Person2, _Person1 }));
+        }
+        [Test]
+        public void AddFirt_AddNull_ObjectsInCorrectOrder()
+        {
+            DoubleLinkedList<Person> linkedList = new();
+            linkedList.AddFirst(_Person1);
+            linkedList.AddFirst(_PersonNull);
+            Assert.That(linkedList.GetAllNodesData(), Is.EqualTo(new List<Person?> { _PersonNull, _Person1 }));
+        }
+        [Test]
+        public void AddLast_AddMultipleObjects_ObjectsInCorrectOrder()
+        {
+            DoubleLinkedList<Person> linkedList = new();
+            linkedList.AddLast(_Person1);
+            linkedList.AddLast(_Person2);
+            linkedList.AddLast(_Person3);
+            Assert.That(linkedList.GetAllNodesData(), Is.EqualTo(new List<Person> { _Person1, _Person2, _Person3 }));
+        }
+        [Test]
+        public void AddLast_AddNull_ObjectsInCorrectOrder()
+        {
+            DoubleLinkedList<Person> linkedList = new();
+            linkedList.AddLast(_Person1);
+            linkedList.AddLast(_PersonNull);
+            Assert.That(linkedList.GetAllNodesData(), Is.EqualTo(new List<Person?> { _Person1, _PersonNull }));
+        }
+        [Test]
+        public void AddLast_ListEmptyAddFirst_LastCorrectlyInitialized()
+        {
+            DoubleLinkedList<Person> linkedList = new();
+            linkedList.AddFirst(_Person1);
+            linkedList.AddLast(_Person2);
+            linkedList.AddFirst(_Person3);
+            Assert.That(linkedList.GetAllNodesData(), Is.EqualTo(new List<Person> { _Person3, _Person1, _Person2}));
+        }
+    }
+}
