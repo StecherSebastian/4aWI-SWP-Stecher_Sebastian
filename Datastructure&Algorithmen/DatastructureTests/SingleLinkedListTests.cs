70a9b4d3b99ad3b650c2bcbd1409935636e7312e
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2025-11-14 18:12:09 +0100

    addedTestForMethodSetLinkedList

--- Datastructure&Algorithmen/DatastructureTests/SingleLinkedListTests.cs
+++ Datastructure&Algorithmen/DatastructureTests/SingleLinkedListTests.cs
@@ -134,6 +134,16 @@ namespace DatastructureTests
             Assert.That(linkedList.GetAllNodesData(), Is.EqualTo(new List<Person> { _PersonNull, _Person1 }));
         }
         [Test]
+        public void Set_ValidPosition_UpdatesNodeData()
+        {
+            SingleLinkedList<Person> linkedList = new();
+            linkedList.AddFirst(_Person1);
+            linkedList.AddLast(_Person2);
+            linkedList.AddLast(_Person3);
+            linkedList.Set(1, _PersonNull);
+            Assert.That(linkedList.GetAllNodesData(), Is.EqualTo(new List<Person?> { _Person1, _PersonNull, _Person3 }));
+        }
+        [Test]
         public void Count_AddingMultipleObjects_ReturnsCorrectNumber()
         {
             SingleLinkedList<Person> linkedList = new();
