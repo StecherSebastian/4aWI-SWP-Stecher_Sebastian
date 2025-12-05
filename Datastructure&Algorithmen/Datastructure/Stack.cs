c5d9889acde59c6b52868cc19de4cbb820efc753
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2025-12-05 13:55:23 +0100

    addedUnitTests

--- Datastructure&Algorithmen/Datastructure/Stack.cs
+++ Datastructure&Algorithmen/Datastructure/Stack.cs
@@ -9,11 +9,21 @@
         }
         public void Push(T data) =>
             _List.AddLast(data);
-        public void Pop() =>
+        public T Pop()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Cannot pop from empty stack");
+            T removed = _List.Get(Size() - 1);
             _List.RemoveLast();
+            return removed;
+        }
         public int Size() =>
             _List.Count();
         public bool IsEmpty() =>
             _List.Count() == 0;
+        public T Top()
+        {
+            return _List.Get(Size() - 1);
+        }
     }
 }
---
+++ Datastructure&Algorithmen/DatastructureTests/StackUnitTests.cs
@@ -0,0 +1,52 @@
+﻿using Common;
+
+namespace DatastructureTests
+{
+    public class StackUnitTests
+    {
+        private Person _Person1;
+        private Person _Person2;
+        private Person _Person3;
+        private Person? _PersonNull;
+        [SetUp]
+        public void Setup()
+        {
+            _Person1 = new("Sebastian");
+            _Person2 = new("Lukas");
+            _Person3 = new("Stephan");
+            _PersonNull = null;
+        }
+        [Test]
+        public void Push_AddSeveralObject_ObjectsInStack()
+        {
+            Datastructure.Stack<Person> stack = new();
+            stack.Push(_Person1);
+            stack.Push(_Person2);
+            stack.Push(_Person3);
+            stack.Push(_PersonNull);
+            Assert.Multiple(() =>
+            {
+                Assert.That(stack.Pop(), Is.EqualTo(_PersonNull));
+                Assert.That(stack.Pop(), Is.EqualTo(_Person3));
+                Assert.That(stack.Pop(), Is.EqualTo(_Person2));
+                Assert.That(stack.Pop(), Is.EqualTo(_Person1));
+                Assert.That(stack.IsEmpty(), Is.True);
+            });
+        }
+        [Test]
+        public void Pop_EmptyStack_ThrowInvalidOperationException()
+        {
+            Datastructure.Stack<Person> stack = new();
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+        [Test]
+        public void Size_PushAndPopObjects_ReturnCorrectSize()
+        {
+            Datastructure.Stack<Person> stack = new();
+            stack.Push(_Person1);
+            stack.Push(_Person2);
+            stack.Pop();
+            Assert.That(stack.Size(), Is.EqualTo(1));
+        }
+    }
+}
