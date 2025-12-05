4b71bb5c698a44873bf8d325e55f895365bf7ac7
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2025-12-05 13:10:06 +0100

    addedStack

---
+++ Datastructure&Algorithmen/Datastructure/Stack.cs
@@ -0,0 +1,19 @@
+﻿namespace Datastructure
+{
+    public class Stack<T> where T : IComparable<T>
+    {
+        private DoubleLinkedList<T> _List;
+        public Stack() 
+        {
+            _List = new DoubleLinkedList<T>();
+        }
+        public void Push(T data) =>
+            _List.AddLast(data);
+        public void Pop() =>
+            _List.RemoveLast();
+        public int Size() =>
+            _List.Count();
+        public bool IsEmpty() =>
+            _List.Count() == 0;
+    }
+}
