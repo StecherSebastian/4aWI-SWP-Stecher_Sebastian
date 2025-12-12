9995ccdc7404dae981b507c2621bc2b98f7eb131
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2025-12-12 16:43:21 +0100

    implementedQueue

---
+++ Datastructure&Algorithmen/Datastructure/Queue.cs
@@ -0,0 +1,27 @@
+﻿namespace Datastructure
+{
+    public class Queue<T> where T : IComparable<T>
+    {
+        private SingleLinkedList<T> _List;
+        public Queue() =>
+            _List = new SingleLinkedList<T>();
+        public void Enqueue(T item) =>
+            _List.AddLast(item);
+        public void Dequeue()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Cannot dequeue from empty queue");
+            _List.RemoveFirst();
+        }
+        public T? Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Cannot peek empty queue");
+            return _List.Get(0);
+        }
+        public int Size() =>
+            _List.Count();
+        public bool IsEmpty() =>
+            Size() == 0;
+    }
+}
