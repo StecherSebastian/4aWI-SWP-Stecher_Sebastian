f294e084d262fdea4899381e8cabdb0206820391
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2026-01-18 16:15:00 +0100

    Added more Unit Tests for Bucket Sort

--- Datastructure&Algorithmen/AlgorithmenTests/BucketSortTests.cs
+++ Datastructure&Algorithmen/AlgorithmenTests/BucketSortTests.cs
@@ -1,12 +1,10 @@
 ﻿using Algorithmen;
-using Common;
 
 namespace AlgorithmenTests;
-
 public class BucketSortTests
 {
     [Test]
-    public void Sort_AddMultipleDoubles_DoublesSorted()
+    public void Sort_DoublesInISortableDatastructure_DoublesSorted()
     {
         MockSortable<double> list = new(new[] {4645.34, 123.22342, 23.234, 234.23 });
         BucketSortStrategy<double> sorter = new();
@@ -14,11 +12,27 @@ public class BucketSortTests
         Assert.That(list.ToList(), Is.EqualTo(new List<double> { 23.234, 123.22342, 234.23, 4645.34 }));
     }
     [Test]
-    public void Sort_AddMultipleIntegers_IntegerssSorted()
+    public void Sort_IntegersInISortableDatastructure_IntegerssSorted()
     {
         MockSortable<int> list = new(new[] { 123, 45, 234, 1 });
         BucketSortStrategy<int> sorter = new();
         sorter.Sort(list);
         Assert.That(list.ToList(), Is.EqualTo(new List<int> { 1, 45, 123, 234 }));
+    }
+    [Test]
+    public void Sort_DoublesInIList_DoublesSorted()
+    {
+        IList<double> list = new List<double> { 4645.34, 123.22342, 23.234, 234.23 };
+        var sorter = new BucketSortStrategy<double>();
+        sorter.Sort(list);
+        Assert.That(list, Is.EqualTo(new List<double> { 23.234, 123.22342, 234.23, 4645.34 }));
+    }
+    [Test]
+    public void Sort_IntegersInIList_IntegersSorted()
+    {
+        IList<int> list = new List<int> { 123, 45, 234, 1 };
+        var sorter = new BucketSortStrategy<int>();
+        sorter.Sort(list);
+        Assert.That(list, Is.EqualTo(new List<int> { 1, 45, 123, 234 }));
     }
 }
