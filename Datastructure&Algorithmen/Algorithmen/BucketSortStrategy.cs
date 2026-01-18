a79040de5bef944f8c226f89822e3a98aaa87f83
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2026-01-18 15:58:42 +0100

    Implemented Bucket Sort

---
+++ Datastructure&Algorithmen/Algorithmen/BucketSortStrategy.cs
@@ -0,0 +1,57 @@
+﻿using Common;
+using System.Numerics;
+
+namespace Algorithmen
+{
+    public class BucketSortStrategy<T> : ISortStrategy<T> where T : INumber<T>
+    {
+        private readonly InsertionSortStrategy<T> _InsertionSort;
+
+        public BucketSortStrategy() => _InsertionSort = new();
+
+        public void Sort(ISortableDatastructure<T> d)
+        {
+            if (d == null)
+                throw new ArgumentNullException(nameof(d));
+            int n = d.Count();
+            if (n <= 1)
+                return;
+            T min = d.Get(0)!;
+            T max = d.Get(0)!;
+            for (int i = 1; i < n; i++)
+            {
+                T value = d.Get(i)!;
+                if (value is null)
+                    throw new InvalidOperationException("Null values are not supported for numeric sorting.");
+                if (value < min) min = value;
+                if (value > max) max = value;
+            }
+            if (min == max)
+                return;
+            List<List<T>> buckets = new();
+            for (int i = 0; i < n; i++)
+            {
+                buckets.Add(new List<T>());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                T value = d.Get(i)!;
+                if (value is null)
+                    throw new InvalidOperationException("Null values are not supported for numeric sorting.");
+                double normalized = double.CreateTruncating(value - min) / double.CreateTruncating(max - min + T.One);
+                int bi = (int)(normalized * n);
+                if (bi == n) bi = n - 1;
+                buckets[bi].Add(value);
+            }
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                buckets[i].Sort();
+                for (int j = 0; j < buckets[i].Count(); j++)
+                {
+                    d.Set(index++, buckets[i][j]);
+                }
+            }
+        }
+    }
+}
---
+++ Datastructure&Algorithmen/AlgorithmenTests/BucketSortTests.cs
@@ -0,0 +1,24 @@
+﻿using Algorithmen;
+using Common;
+
+namespace AlgorithmenTests;
+
+public class BucketSortTests
+{
+    [Test]
+    public void Sort_AddMultipleDoubles_DoublesSorted()
+    {
+        MockSortable<double> list = new(new[] {4645.34, 123.22342, 23.234, 234.23 });
+        BucketSortStrategy<double> sorter = new();
+        sorter.Sort(list);
+        Assert.That(list.ToList(), Is.EqualTo(new List<double> { 23.234, 123.22342, 234.23, 4645.34 }));
+    }
+    [Test]
+    public void Sort_AddMultipleIntegers_IntegerssSorted()
+    {
+        MockSortable<int> list = new(new[] { 123, 45, 234, 1 });
+        BucketSortStrategy<int> sorter = new();
+        sorter.Sort(list);
+        Assert.That(list.ToList(), Is.EqualTo(new List<int> { 1, 45, 123, 234 }));
+    }
+}
