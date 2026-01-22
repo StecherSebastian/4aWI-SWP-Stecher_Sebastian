717f69165c1604753fb14e169f257d46e05469d2
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2026-01-22 11:50:27 +0100

    Added Unit Test for Quick Sort

---
+++ Datastructure&Algorithmen/AlgorithmenTests/QuickSortTests.cs
@@ -0,0 +1,19 @@
+﻿using Algorithmen;
+using Common;
+
+namespace AlgorithmenTests;
+public class QuickSortTests
+{
+    [Test]
+    public void Sort_AddMultipleObjects_ObjectsSorted()
+    {
+        Person person1 = new("Sebastian");
+        Person person2 = new("Lukas");
+        Person person3 = new("Stephan");
+        Person? personNull = null;
+        MockSortable<Person> linkedList = new(new[] { person1, person2, person3, personNull });
+        QuickSortStrategy<Person> sorter = new();
+        sorter.Sort(linkedList);
+        Assert.That(linkedList.ToList(), Is.EqualTo(new List<Person?> { personNull, person2, person1, person3 }));
+    }
+}
